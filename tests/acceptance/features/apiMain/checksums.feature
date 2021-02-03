fd221f83401ef42ba6ad0055c846a4af97cdc0b7
swoichha <swoichhaa@gmail.com>
2021-02-03 15:57:40 +0545

    fix php-style

--- tests/acceptance/features/apiMain/checksums.feature
+++ tests/acceptance/features/apiMain/checksums.feature
@@ -333,7 +333,7 @@ Feature: checksums
 
   Scenario Outline: Uploading a file with checksum should work for file with special characters
     When user "Alice" uploads file "filesForUpload/textfile.txt" to <renamed_file> with checksum "MD5:d70b40f177b14b470d1756a3c12b963a" using the WebDAV API
-    Then the HTTP status code should be "204"
+    Then the HTTP status code should be "201"
     And the content of file <renamed_file> for user "Alice" should be:
       """
       This is a testfile.
--- tests/acceptance/features/bootstrap/WebDavPropertiesContext.php
+++ tests/acceptance/features/bootstrap/WebDavPropertiesContext.php
@@ -679,10 +679,8 @@ class WebDavPropertiesContext implements Context {
 			isset($xmlPart[0]), "Cannot find item with xpath \"$xpath\""
 		);
 		$user = $this->featureContext->getActualUsername($user);
-		$value = strtolower($xmlPart[0]->__toString());
-		$pattern = strtolower($this->featureContext->substituteInLineCodes(
-			$pattern, $user, ['preg_quote' => ['/']])
-		);
+		$value = \strtolower($xmlPart[0]->__toString());
+		$pattern = \strtolower($this->featureContext->substituteInLineCodes($pattern, $user, ['preg_quote' => ['/']]));
 		Assert::assertRegExp(
 			$pattern, $value,
 			"item \"$xpath\" found with value \"$value\", " .
